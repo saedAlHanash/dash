1723123eb782f96b73ca948aa264b39ce2fa550e
saed <samersame404@gmail.com>
2023-10-03 00:33:33 +0300

    add search filter to table

--- lib/core/util/file_util.dart
+++ lib/core/util/file_util.dart
@@ -7,6 +7,7 @@ import 'package:collection/collection.dart';
 import 'package:drawable_text/drawable_text.dart';
 import 'package:excel/excel.dart';
 import 'package:http/http.dart' as http;
+import 'package:qareeb_dash/core/api_manager/api_service.dart';
 
 saveXls(
     {required List<String> header, required List<List<dynamic>> data, String? fileName}) {
@@ -115,7 +116,8 @@ saveImageFile({
 
 Future<Uint8List?> fetchImage(String imageUrl) async {
   if (imageUrl.isEmpty) return null;
-  final response = await http.get(Uri.parse(imageUrl));
+  final response = await APIService()
+      .getApi(url: imageUrl.replaceAll('https://live.qareeb-maas.com/', ''));
   if (response.statusCode == 200) {
     return response.bodyBytes;
   } else {
