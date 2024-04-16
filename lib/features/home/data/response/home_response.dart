14faf58b0da165f9452a2b4cdd50a7d35c1cb678
saed <samersame404@gmail.com>
2024-04-16 13:46:28 +0300

    add rolles and permitions

--- lib/features/home/data/response/home_response.dart
+++ lib/features/home/data/response/home_response.dart
@@ -25,12 +25,12 @@ class HomeResult {
 
   factory HomeResult.fromJson(Map<String, dynamic> json) {
     return HomeResult(
-      statistics: Statistics.fromJson(json["statistics"] ?? {}),
+      statistics: Statistics.fromJson(json["Statistics"] ?? {}),
     );
   }
 
   Map<String, dynamic> toJson() => {
-        "statistics": statistics.toJson(),
+        "Statistics": statistics.toJson(),
       };
 }
 
@@ -61,29 +61,29 @@ class Statistics {
 
   factory Statistics.fromJson(Map<String, dynamic> json) {
     return Statistics(
-      clients: json["clients"] ?? 0,
-      drivers: json["drivers"] ?? 0,
-      trips: json["trips"] ?? 0,
-      sharedTrips: json["sharedTrips"] ?? 0,
-      activeDrivers: json["activeDrivers"] ?? 0,
-      institutions: json["institutions"] ?? 0,
-      agencies: json["agencies"] ?? 0,
-      incoms: json["incoms"] ?? 0,
-      awards: json["awards"] ?? 0,
-      requiredAmountFromComapnay: json["requiredAmountFromComapnay"] ?? 0,
+      clients: json["Clients"] ?? 0,
+      drivers: json["Drivers"] ?? 0,
+      trips: json["Trips"] ?? 0,
+      sharedTrips: json["SharedTrips"] ?? 0,
+      activeDrivers: json["ActiveDrivers"] ?? 0,
+      institutions: json["Institutions"] ?? 0,
+      agencies: json["Agencies"] ?? 0,
+      incoms: json["Incoms"] ?? 0,
+      awards: json["Awards"] ?? 0,
+      requiredAmountFromComapnay: json["RequiredAmountFromComapnay"] ?? 0,
     );
   }
 
   Map<String, dynamic> toJson() => {
-        "clients": clients,
-        "drivers": drivers,
-        "trips": trips,
-        "sharedTrips": sharedTrips,
-        "activeDrivers": activeDrivers,
-        "institutions": institutions,
-        "agencies": agencies,
-        "incoms": incoms,
-        "awards": awards,
-        "requiredAmountFromComapnay": requiredAmountFromComapnay,
+        "Clients": clients,
+        "Drivers": drivers,
+        "Trips": trips,
+        "SharedTrips": sharedTrips,
+        "ActiveDrivers": activeDrivers,
+        "Institutions": institutions,
+        "Agencies": agencies,
+        "Incoms": incoms,
+        "Awards": awards,
+        "RequiredAmountFromComapnay": requiredAmountFromComapnay,
       };
 }
